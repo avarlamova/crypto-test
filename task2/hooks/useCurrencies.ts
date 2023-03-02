24737fe40bdb03c377df75654e595f190e1bad7e
avarlamova <59831804+avarlamova@users.noreply.github.com>
2023-03-02 22:23:09 +0300

    feath: toAth, fromAth

--- task2/hooks/useCurrencies.ts
+++ task2/hooks/useCurrencies.ts
@@ -1,28 +1,29 @@
+import { fetchAth, calculateAth } from "@/../task1/allTimeHigh";
 import React, { useState, useEffect } from "react";
 const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 import { FetchedCurrencyOption } from "../types/types";
 
 const useCurrencies = () => {
-  const [currencies, setCurrencies] = useState([]);
+  const [currencies, setCurrencies] = useState<any>([]);
 
   useEffect(() => {
-    fetch(`https://api.cryptorank.io/v1/currencies?api_key=${API_KEY}`).then(
-      (response) =>
-        response.json().then((data) => {
-          const formattedData = data.data.map((el: FetchedCurrencyOption) => {
-            return {
-              //   id: el.id,
-              //   name: el.name,
-              //   category: el.category,
-              //   circulatingSupply: el.circulatingSupply,
-              value: el.values.USD.price,
-              marketCap: el.values.USD.marketCap,
-              ...el,
-            };
-          });
-          setCurrencies(formattedData);
-        })
-    );
+    fetch(`https://api.cryptorank.io/v1/currencies?api_key=${API_KEY}`)
+      .then((response) => response.json())
+      .then(async (data) => {
+        const formattedData = data.data.map(async (el: any) => {
+          // const getUsers = async ({ ids }) => {
+          //     const users = ids.map(async id => {
+          //         const { data } = await api.request(`/${id}`);
+          //         return data;
+          //     });
+          //     return Promise.all(users);
+          // }
+
+          return { ...el, ath: await fetchAth(el.slug) };
+        });
+        const res = await Promise.all(formattedData);
+        setCurrencies(res);
+      });
   }, []);
   return currencies;
 };
--- task2/pages/watchlist/index.tsx
+++ task2/pages/watchlist/index.tsx
@@ -1,23 +1,31 @@
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
-import { fetchAth } from "../../../task1/allTimeHigh";
+import { calculateAth } from "../../../task1/allTimeHigh";
 import useCurrencies from "@/hooks/useCurrencies";
+import { CurrencyOption, FetchedCurrencyOption } from "@/types/types";
 
 type TableProps = {
   label: string;
   value: string;
 };
 
-type CurrencyProps = {
-  id: number;
-  name: string;
-  circulatingSupply: number;
-  marketCap: number;
-  category: string;
-};
-
 const CurrenciesTable = ({ label, value }: TableProps) => {
   const currencies = useCurrencies();
+
+  //   useEffect(() => {
+  //     const getData = async () => {
+  //       const fileData = await Promise.all(
+  //         currencies.map(async (el: FetchedCurrencyOption) => {
+  //           const { currentAth, currentPrice } = await fetchAth(el.slug);
+  //           return { test: 1, currentAth, currentPrice };
+  //         })
+  //       );
+  //       setTest(fileData);
+  //       console.log(test);
+  //     };
+  //     getData();
+  //   }, [currencies]);
+
   const EditableTable = () => {
     return (
       <>
@@ -35,7 +43,20 @@ const CurrenciesTable = ({ label, value }: TableProps) => {
           </thead>
           <tbody>
             {currencies.map(
-              ({ id, name, circulatingSupply, category, value, marketCap }) => {
+              ({
+                id,
+                name,
+                circulatingSupply,
+                category,
+                value,
+                marketCap,
+                ath,
+              }) => {
+                const { currentAth, currentPrice } = ath;
+                const { fromAth, toAth } = calculateAth(
+                  currentPrice,
+                  currentAth
+                );
                 return (
                   <tr key={id}>
                     <td>{name}</td>
@@ -43,6 +64,8 @@ const CurrenciesTable = ({ label, value }: TableProps) => {
                     <td>{circulatingSupply}</td>
                     <td>{marketCap}</td>
                     <td>{category}</td>
+                    <td>{fromAth}</td>
+                    <td>{toAth}</td>
                   </tr>
                 );
               }
